d846b56957b5d258d2df391671511e46da458299
alexisdevelops <alexisdevelops@gmail.com>
2021-10-07 17:53:47 -0700

    Initial Commit

---
+++ Fastlane-Demo/Fastlane_Demo.h
@@ -0,0 +1,18 @@
+//
+//  Fastlane_Demo.h
+//  Fastlane-Demo
+//
+//  Created by alexis on 07/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Fastlane_Demo.
+FOUNDATION_EXPORT double Fastlane_DemoVersionNumber;
+
+//! Project version string for Fastlane_Demo.
+FOUNDATION_EXPORT const unsigned char Fastlane_DemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fastlane_Demo/PublicHeader.h>
+
+
