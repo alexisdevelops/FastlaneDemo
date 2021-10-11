3b707e695fdf6a595501431fa358bab216140549
alexisdevelops <alexisdevelops@gmail.com>
2021-10-10 19:46:36 -0700

    Initial commit

---
+++ Helper/Printer.swift
@@ -0,0 +1,14 @@
+//
+//  Printer.swift
+//  Fastlane-Demo
+//
+//  Created by alexis on 07/10/21.
+//
+
+import Foundation
+
+class Printer{
+    func printStuf(){
+        print("Just swift things")
+    }
+}
